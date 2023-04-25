123dcab9dfac4605f3740f0aab5d344723488108
Anton <toscha.korol@yandex.ru>
2023-04-25 23:05:55 +0300

    Task2

--- Homeworks1/Program.cs
+++ Homeworks1/Program.cs
@@ -7,14 +7,14 @@
 // Console.WriteLine(a > b ? "Первое число больше второго" : "Второе число больше первого");
 
 // Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-Console.WriteLine("Число A:");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Число B:");
-int b = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Число C:");
-int c = Convert.ToInt32(Console.ReadLine());
-int max = a;
-if (max < b) max = b;
-if (max < c) max = c;
+// Console.WriteLine("Число A:");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Число B:");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Число C:");
+// int c = Convert.ToInt32(Console.ReadLine());
+// int max = a;
+// if (max < b) max = b;
+// if (max < c) max = c;
 
-System.Console.WriteLine("Максимальное из этих чисел = "  + max);
+// System.Console.WriteLine("Максимальное из этих чисел = "  + max);
